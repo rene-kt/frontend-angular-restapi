bc51b879561e7f58f0fbf8f659ac6820d6895d80
Renê <renejr.arraes286@gmail.com>
2020-11-11 09:08:54 -0300

    finished validations and rest methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,7 +56,27 @@ export class AppComponent implements OnInit {
     this.productService.saveProduct(this.product).subscribe(() => {
       this.getProducts();
       this.productForm.reset();
+    });
+  }
 
+  updateItem(): void {
+    this.selectedProduct.id = this.selectedProduct.id;
+
+    if (this.productForm.value.name !== '') {
+      this.selectedProduct.name = this.productForm.value.name;
+    }
+
+    if (this.productForm.value.value !== '') {
+      this.selectedProduct.value = this.productForm.value.value;
+    }
+
+    if (this.productForm.value.quantity !== '') {
+      this.selectedProduct.quantity = this.productForm.value.quantity;
+    }
+
+    this.productService.updateProduct(this.selectedProduct).subscribe(() => {
+      this.editMode = false;
+      this.productForm.reset();
     });
   }
 
@@ -74,17 +94,8 @@ export class AppComponent implements OnInit {
     this.editMode = condition;
     this.selectedProduct = product;
 
-  }
-  updateItem(): void {
-
-    this.selectedProduct.id = this.selectedProduct.id;
-    this.selectedProduct.name = this.productForm.value.name;
-    this.selectedProduct.value = this.productForm.value.value;
-    this.selectedProduct.quantity = this.productForm.value.quantity;
-
-    this.productService.updateProduct(this.selectedProduct).subscribe(() => {
-      this.editMode = false;
-      this.productForm.reset();
-    });
+    if(condition === false){
+      this.selectedProduct.name = '';
+    }
   }
 }
